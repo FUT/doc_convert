0dc922f91d7ec73b98125465dfb1f86072cb4903
Yury Kaliada <fut.wrk@gmail.com>
2014-01-27 13:17:08 +0300

    process multiple files at once
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- convert.rb
+++ convert.rb
@@ -2,18 +2,18 @@ require 'RMagick'
 require 'pry'
 require 'htmltoword'
 
-pdf_file_name = "test.pdf"
-original_pdf = File.open(pdf_file_name, 'rb').read
-images = Magick::Image.from_blob(original_pdf) do
-  self.format = 'PDF'
-  self.quality = 100
-  self.density = 144
-end
+%w{agreement annex correct}.each do |file|
+  original_pdf = File.open("tmp/#{file}.pdf", 'rb').read
+  images = Magick::Image.from_blob(original_pdf) do
+    self.format = 'PDF'
+    self.quality = 100
+    self.density = 144
+  end
 
-`rm *.png`
-images.each_with_index do |image, i|
-  image.format = 'JPG'
-  image.to_blob
-  image.write("page_#{i}.png")
+  images.each_with_index do |image, i|
+    image.format = 'JPG'
+    image.to_blob
+    image.write("tmp/_#{file}_#{i}.png")
+  end
 end
 
